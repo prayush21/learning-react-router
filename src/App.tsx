5d1e0edc27566de24dd2293600f50f3f01b4e4e1
Prayush Dave <prayushdave@gmail.com>
2023-02-26 20:01:43 +0530

    Complete Basic Usage

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,32 @@
 import { useState } from "react";
+import { Link, Route, Routes } from "react-router-dom";
+import BookList from "./pages/BookList";
+import Home from "./pages/Home";
+import NewBook from "./pages/NewBook";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <h1>Welcome</h1>;
+  return (
+    <>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/books">Books</Link>
+            {/* <a href="/books">Books</a> */}
+          </li>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/books" element={<BookList />} />
+        <Route />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
